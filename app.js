45e67fb77d4c97e58f4e20f69e9ddfc57d8b404a
Edward Gao <egao@sent.com>
2017-03-04 10:10:10 -1000

    test retrieve from memory

--- app.js
+++ app.js
@@ -2,4 +2,7 @@ window.onload = function() {
 	console.log(memoize.query('h1'));
 	console.log(memoize.query('.title'));
 	console.log(memoize.id('lol'));
+	console.log(memoize.query('h1'));
+	console.log(memoize.query('.title'));
+	console.log(memoize.id('lol'));
 }
